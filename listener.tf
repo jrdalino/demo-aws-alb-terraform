ee02a8a54002227ceca5be802fed30a3d846e67b
Jose Dalino <3316034+jrdalino@users.noreply.github.com>
2020-07-06 10:41:44 +0800

    Create listener.tf

---
+++ listener.tf
@@ -0,0 +1,18 @@
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+  # ssl_policy        = "ELBSecurityPolicy-2016-08"
+  # certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+
+  }
+}
