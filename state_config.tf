6121e420d011bcfd5bbf23e9bda5f1bd8bb3bfd4
Jose Dalino <3316034+jrdalino@users.noreply.github.com>
2020-07-06 10:43:26 +0800

    Create state_config.tf

---
+++ state_config.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    region         = "ap-southeast-1"
+    bucket         = "707538076348-ap-southeast-1-terraform-state"
+    key            = "alb.tfstate"
+    encrypt        = "true"
+    dynamodb_table = "terraform-state-lock"
+  }
